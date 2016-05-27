130ad0b1e1c19a17e6ce83a581d10fecd6f62601
Saienko Nicolas <Saienko Nicolas>
2016-05-27 15:28:46 +0300

    empty proj

---
+++ lr2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿/// <summary>
+/// queuing system
+/// </summary>
+namespace Lr2
+{
+using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
